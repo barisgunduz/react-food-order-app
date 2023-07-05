34b3484d37a47100c363d81c3ffdabd11d7da9d6
Barış Gündüz <baris@gunduzmedya.com>
2023-07-05 10:05:57 +0300

    Feature - Submitting sending card data

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -6,7 +6,7 @@ const isEmpty = (value) => value.trim() === "";
 const isFiveChars = (value) => value.trim().length === 5;
 
 const Checkout = (props) => {
-    const [formInputsValidty, setFormInputsValidty] = useState({
+    const [formInputsValidity, setFormInputsValidity] = useState({
         name: true,
         street: true,
         city: true,
@@ -31,7 +31,7 @@ const Checkout = (props) => {
         const enteredCityIsValid = !isEmpty(enteredCity);
         const enteredPostalCodeIsValid = isFiveChars(enteredPostalCode);
 
-        setFormInputsValidty({
+        setFormInputsValidity({
             name: enteredNameIsValid,
             street: enteredStreetIsValid,
             city: enteredCityIsValid,
@@ -48,20 +48,25 @@ const Checkout = (props) => {
             return;
         }
 
-        // Submit cart data
+        props.onConfirm({
+            name: enteredName,
+            street: enteredStreet,
+            city: enteredCity,
+            postalCode: enteredPostalCode,
+        });
     };
 
     const nameControlClasses = `${classes.control} ${
-        formInputsValidty.name ? "" : classes.invalid
+        formInputsValidity.name ? "" : classes.invalid
     }`;
     const streetControlClasses = `${classes.control} ${
-        formInputsValidty.street ? "" : classes.invalid
-    }`;
-    const cityControlClasses = `${classes.control} ${
-        formInputsValidty.city ? "" : classes.invalid
+        formInputsValidity.street ? "" : classes.invalid
     }`;
     const postalCodeControlClasses = `${classes.control} ${
-        formInputsValidty.postalCode ? "" : classes.invalid
+        formInputsValidity.postalCode ? "" : classes.invalid
+    }`;
+    const cityControlClasses = `${classes.control} ${
+        formInputsValidity.city ? "" : classes.invalid
     }`;
 
     return (
@@ -69,26 +74,26 @@ const Checkout = (props) => {
             <div className={nameControlClasses}>
                 <label htmlFor="name">Your Name</label>
                 <input type="text" id="name" ref={nameInputRef} />
-                {!formInputsValidty.name && <p>Please enter a valid name!</p>}
+                {!formInputsValidity.name && <p>Please enter a valid name!</p>}
             </div>
             <div className={streetControlClasses}>
                 <label htmlFor="street">Street</label>
                 <input type="text" id="street" ref={streetInputRef} />
-                {!formInputsValidty.street && (
+                {!formInputsValidity.street && (
                     <p>Please enter a valid street!</p>
                 )}
             </div>
             <div className={postalCodeControlClasses}>
                 <label htmlFor="postal">Postal Code</label>
                 <input type="text" id="postal" ref={postalCodeInputRef} />
-                {!formInputsValidty.postalCode && (
-                    <p>Please enter a valid postal code!</p>
+                {!formInputsValidity.postalCode && (
+                    <p>Please enter a valid postal code (5 characters long)!</p>
                 )}
             </div>
             <div className={cityControlClasses}>
                 <label htmlFor="city">City</label>
                 <input type="text" id="city" ref={cityInputRef} />
-                {!formInputsValidty.city && <p>Please enter a valid city!</p>}
+                {!formInputsValidity.city && <p>Please enter a valid city!</p>}
             </div>
             <div className={classes.actions}>
                 <button type="button" onClick={props.onCancel}>
